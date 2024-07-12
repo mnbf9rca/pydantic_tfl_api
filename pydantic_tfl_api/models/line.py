5639a88deedb9cb89da1dc4dcf7bd8514540286f
Rob Aleck <mnbf9rca@users.noreply.github.com>
2024-07-12 08:34:20 +0000

    chore: Update Line model field defaults and aliases

--- pydantic_tfl_api/models/line.py
+++ pydantic_tfl_api/models/line.py
@@ -11,13 +11,13 @@ from .crowding import Crowding
 class Line(BaseModel):
     id: str = Field(alias='id')
     name: str = Field(alias='name')
-    mode_name: str = Field(alias='modeName')
+    mode_name: str = Field(None, alias='modeName')
     disruptions: Optional[List[Disruption]] = Field([], alias='disruptions')
     created: Optional[datetime] = Field(None, alias='created')
     modified: Optional[datetime] = Field(None, alias='modified')
-    line_statuses: List[LineStatus] = Field(alias='lineStatuses')
-    route_sections: List[RouteSection] = Field(alias='routeSections')
-    service_types: List[ServiceType] = Field(alias='serviceTypes')
+    line_statuses: List[LineStatus] = Field(None, alias='lineStatuses')
+    route_sections: List[RouteSection] = Field(None, alias='routeSections')
+    service_types: List[ServiceType] = Field(None, alias='serviceTypes')
     crowding: Optional[Crowding] = Field(None, alias='crowding')
 
     model_config = {'populate_by_name': True}
