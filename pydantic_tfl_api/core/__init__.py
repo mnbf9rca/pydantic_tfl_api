13d0bc1fc14bf1644fecd427bfbbf02b1f5f5a6a
Rob Aleck <mnbf9rca@users.noreply.github.com>
2025-11-18 16:18:28 +0000

    rebuild with async clients

--- pydantic_tfl_api/core/__init__.py
+++ pydantic_tfl_api/core/__init__.py
@@ -1,9 +1,25 @@
+from .async_client import AsyncClient
+from .async_rest_client import AsyncRestClient
 from .client import Client
 from .http_backends import RequestsClient
-from .http_client import HTTPClientBase, HTTPResponse
+from .http_client import (
+    AsyncHTTPClientBase,
+    HTTPClientBase,
+    HTTPResponse,
+    get_default_async_http_client,
+    get_default_http_client,
+)
 from .package_models import ApiError, GenericResponseModel, ResponseModel
 from .response import UnifiedResponse
 from .rest_client import RestClient
+
+# Optional httpx imports - only available if httpx is installed
+try:
+    from .http_backends import AsyncHttpxClient, HttpxClient  # noqa: F401
+
+    _httpx_available = True
+except ImportError:
+    _httpx_available = False
 
 # Runtime version discovery from installed package metadata
 try:
@@ -19,10 +35,19 @@ __all__ = [
     "ResponseModel",
     "GenericResponseModel",
     "Client",
+    "AsyncClient",
     "RestClient",
+    "AsyncRestClient",
     "HTTPClientBase",
+    "AsyncHTTPClientBase",
     "HTTPResponse",
     "RequestsClient",
     "UnifiedResponse",
+    "get_default_http_client",
+    "get_default_async_http_client",
     "__version__",
 ]
+
+# Add httpx clients to __all__ if available
+if _httpx_available:
+    __all__ += ["HttpxClient", "AsyncHttpxClient"]
--- pydantic_tfl_api/endpoints/AirQualityClient.py
+++ pydantic_tfl_api/endpoints/AirQualityClient.py
@@ -1,4 +1,4 @@
-from ..core import ApiError, Client, ResponseModel
+from ..core import ApiError, AsyncClient, Client, ResponseModel
 from ..models import LondonAirForecast
 from .AirQualityClient_config import base_url, endpoints
 
@@ -19,4 +19,22 @@ class AirQualityClient(Client):
         No parameters required.
         '''
         return self._send_request_and_deserialize(base_url, endpoints['AirQuality_Get'], endpoint_args=None)
+
+
+class AsyncAirQualityClient(AsyncClient):
+    """APIs relating to AirQuality and similar services"""
+
+    async def Get(self, ) -> ResponseModel[LondonAirForecast] | ApiError:
+        '''
+        Gets air quality data feed
+
+  Query path: `/AirQuality/`
+
+  `ResponseModel.content` contains `models.LondonAirForecast` type.
+
+
+  Parameters:
+        No parameters required.
+        '''
+        return await self._send_request_and_deserialize(base_url, endpoints['AirQuality_Get'], endpoint_args=None)
 
